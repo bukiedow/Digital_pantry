ba88f81204b9ede171ad69014971b9c2d21e07be
bukiedow <dowbukie@gmail.com>
2021-11-29 14:23:59 +0000

    edit py mongo

--- env_sample.py
+++ env_sample.py
@@ -3,6 +3,6 @@ import os
 
 os.environ.setdefault("IP", "0.0.0.0")
 os.environ.setdefault("PORT", "5000")
-os.environ.setdefault("SECRET_KEY", "")
+os.environ.setdefault("SECRET_KEY", "e56f7gh8")
 os.environ.setdefault("MONGO_URI", "mongodb+srv://dow:a1b2@digitalpantry.ypv15.mongodb.net/digital_pantry?retryWrites=true&w=majority")
 os.environ.setdefault("MONGO_DBNAME", "digital_pantry")
