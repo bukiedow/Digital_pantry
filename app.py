0e01186b397a2b03c47d45b19ef57f9dd5fa2471
bukiedow <dowbukie@gmail.com>
2021-11-30 23:42:58 +0000

    delete button enabled

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/get_items")
 def get_items():
-    items = mongo.db.items.find()
+    items = list(mongo.db.items.find())
     return render_template("items.html", items=items)
 
 
@@ -123,10 +123,28 @@ def add_item():
     
 @app.route("/edit_item/<item_id>", methods=["GET", "POST"])
 def edit_item(item_id):
+    if request.method == "POST":
+        submit = {
+            "category_name": request.form.get("category_name"),
+            "item_name": request.form.get("item_name"),
+            "item_description": request.form.get("item_description"),
+            "purchase_date": request.form.get("purchase_date"),
+            "item_quantity": request.form.get("item_quantity"),
+            "created_by": session["user"]
+        }
+        mongo.db.items.update({"_id": ObjectId(item_id)}, submit)
+        flash("Item Successfully Updated")
+
     item = mongo.db.items.find_one({"_id": ObjectId(item_id)})
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("edit_item.html", item=item, categories=categories)
 
+
+@app.route("/delete_item/<item_id>")
+def delete_item(item_id):
+    mongo.db.items.remove({"_id": ObjectId(item_id)})
+    flash("Item Successfully Deleted")
+    return redirect(url_for("get_items"))
 
 
 if __name__ == "__main__":
