a9fb6c3e04c0c60b18678a0187c4e897433e0376
bukiedow <dowbukie@gmail.com>
2021-11-30 00:03:38 +0000

    update pantry list

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
     $('.sidenav').sidenav({edge: 'right' });
     $('.collapsible').collapsible();
+    $(".datepicker").datepicker({
+      format: "dd mmmm, yyyy",
+      yearRange: 2,
+      showClearBtn: true,
+      i18n: {
+          done: "Select"
+      }
   });
+});
+
 
